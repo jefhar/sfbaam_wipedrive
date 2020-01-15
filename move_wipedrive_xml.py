41108bcd138718d1addda18b63da0c20beb85109
Jeff Harris <jeff@c11k.com>
2020-01-15 13:38:58 -0800

    Added command line options.

--- move_wipedrive_xml.py
+++ move_wipedrive_xml.py
@@ -1,52 +1,86 @@
+#!/usr/bin/python3
+import getopt
 import os
+import sys
 import xml.etree.ElementTree as ET
 
 
-def file_to_path(_file_name):
+def file_to_path(_file_name, _partitions):
     _directory = '/'
-    for x in range(0, 4):
+    for x in range(0, _partitions):
         _directory = _directory + _file_name[x] + '/'
     return _directory
 
 
-regular_text = '\033[1;m'
-success_text = '\033[1;32m'
-error_text = '\033[1;41m'
-info_text = '\033[1;33m'
+def usage():
+    print('usage: move_wipedrive_xml.py -s <sourcePath> -o <outputPath> -p <partitions> [-f]')
 
-result_color = {'2': success_text,
-                '5': error_text
-                }
 
-path = '.' # todo: make this default to command line argument #1
-dirs = os.listdir(os.getcwd())
-for file in dirs:
-    if file.endswith('xml'):
-        print('Processing ' + file)
-        tree = ET.parse(file)
-        Reports = tree.getroot()
-        Job = Reports.find('Report').find('Jobs').find('Job')
-        size = Job.find('Operation').find('Gigabytes').text
-        directory = file_to_path(file)
-        directory = './tmp/' + size + directory
-        new_file = directory + file
-        # print('Create `' + directory + '`')
-        # print('Move `' + file + '` to `' +newfile +'`')
-        print("\n+" + new_file, end='')
-        print(' - ' + size + 'GB drives.')
-        for Operation in Job.findall('Operation'):
-            Gigabytes = Operation.find('Gigabytes')
-            Serial = Operation.find('Serial')
-            ActionResult = Operation.find('ActionResult')
-            result = "Success" if (ActionResult.attrib['Index'] == '2') else "Failure"
+def main():
+    regular_text = '\033[1;m'
+    success_text = '\033[1;32m'
+    error_text = '\033[1;41m'
+    info_text = '\033[1;33m'
+    result_color = {'2': success_text,
+                    '5': error_text
+                    }
+    output_path = './tmp/'
+    source_path = '.'
+    partitions = 4
+    force_overwrite = False
 
-            print("|--> " + Serial.text + ' - ' + Gigabytes.text + "GB; Result: ", end='')
-            print (result_color.get(ActionResult.attrib['Index'], info_text) + result + regular_text)
+    try:
+        opts, args = getopt.getopt(sys.argv[1:], "hfs:o:p:", ['help', 'force', 'sourcepath=', 'outputpath=', 'partitions='])
+    except getopt.GetoptError as error:
+        print(error)
+        usage()
+        sys.exit(2)
 
-        try:
-            os.makedirs(directory)
-        except:
-            pass
-        os.rename(file, new_file)
+    for opt, arg in opts:
+        if opt in('-h', '--help'):
+            usage()
+            sys.exit()
+        elif opt in('-f', '--force'):
+            force_overwrite = True
+        elif opt in ('-s', '--sourcepath'):
+            source_path = arg.strip()
+        elif opt in('-o', '--outputpath'):
+            output_path = arg.strip() + '/'
+        elif opt in('-p', '--partitions'):
+            partitions = int(arg)
 
-print("\n\nFinished Processing Files.")
+    for file in os.listdir(source_path):
+        file_with_path = source_path + '/' + file
+        if file_with_path.endswith('xml'):
+            print('Processing ' + file_with_path)
+            tree = ET.parse(file_with_path)
+            reports = tree.getroot()
+            job = reports.find('Report').find('Jobs').find('Job')
+            size = job.find('Operation').find('Gigabytes').text
+            output_directory = file_to_path(file, partitions)
+            output_directory = output_path + size + output_directory
+            new_file = output_directory + file
+            # print('Create `' + directory + '`')
+            # print('Move `' + file + '` to `' +newfile +'`')
+            print("\n+" + new_file, end='')
+            print(' - ' + size + 'GB drives.')
+            for Operation in job.findall('Operation'):
+                gigabytes = Operation.find('Gigabytes')
+                drive_serial = Operation.find('Serial')
+                action_result = Operation.find('ActionResult')
+                result = "Success" if (action_result.attrib['Index'] == '2') else "Failure"
+
+                print("|--> " + drive_serial.text + ' - ' + gigabytes.text + "GB; Result: ", end='')
+                print (result_color.get(action_result.attrib['Index'], info_text) + result + regular_text)
+
+            try:
+                os.makedirs(output_directory)
+            except:
+                pass
+            os.rename(file_with_path, new_file) # todo: check for force_overwrite
+
+    print("\n\nFinished Processing Files.")
+
+
+if __name__ == "__main__":
+    main()
