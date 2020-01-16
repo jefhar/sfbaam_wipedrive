27ca1c514cd6e0293defc842dfa566c5f74ae0ed
Jeff Harris <jeff@c11k.com>
2020-01-15 22:47:53 -0800

    Added counts to drive wiping and cosmetic changes.

--- move_wipedrive_xml.py
+++ move_wipedrive_xml.py
@@ -3,7 +3,7 @@ import getopt
 import os
 import sys
 import xml.etree.ElementTree as ET
-import sty
+from sty import rs, fg, bg
 
 
 def file_to_path(_file_name, _partitions):
@@ -14,17 +14,17 @@ def file_to_path(_file_name, _partitions):
 
 
 def usage():
-    print(sty.rs.all + 'usage: move_wipedrive_xml.py -s <sourcePath> -o <outputPath> -p <partitions> [-f]')
+    print(rs.all + 'usage: move_wipedrive_xml.py -s <sourcePath> -o <outputPath> -p <partitions> [-f]')
 
 
 def main():
-    result_color = {'2': sty.fg.green,
-                    '5': sty.bg.red + sty.fg.li_white
-                    }
     output_path = './tmp/'
     source_path = '.'
     partitions = 4
     force_overwrite = False
+    success = 0
+    failure = 0
+    unknown = 0
 
     try:
         opts, args = getopt.getopt(sys.argv[1:], "hfs:o:p:",
@@ -52,38 +52,54 @@ def main():
         if file_with_path.endswith('xml'):
             print('Processing ' + file_with_path)
             tree = ET.parse(file_with_path)
-            reports = tree.getroot()
-            job = reports.find('Report').find('Jobs').find('Job')
+            job = tree.getroot().find('Report').find('Jobs').find('Job')
             size = job.find('Operation').find('Gigabytes').text
-            output_directory = file_to_path(file, partitions)
-            output_directory = output_path + size + output_directory
+
+            output_directory = output_path + size + file_to_path(file, partitions)
             new_file = output_directory + file
             # print('Create `' + directory + '`')
-            # print('Move `' + file + '` to `' +newfile +'`')
+            # print('Move `' + file + '` to `' +new_file +'`')
             print("\n+" + new_file, end='')
             print(' - ' + size + 'GB drives.')
-            for Operation in job.findall('Operation'):
-                gigabytes = Operation.find('Gigabytes')
-                drive_serial = Operation.find('Serial')
-                action_result = Operation.find('ActionResult')
-                result = "Success" if (action_result.attrib['Index'] == '2') else "Failure"
 
-                print("|--> " + drive_serial.text + ' - ' + gigabytes.text + "GB; Result: ", end='')
-                print(result_color.get(action_result.attrib['Index'], sty.fg.cyan) + result + sty.rs.all)
+            for Operation in job.findall('Operation'):
+                gigabytes = Operation.find('Gigabytes').text
+                drive_serial = Operation.find('Serial').text
+                action_result = Operation.find('ActionResult').attrib['Index'].int()
+
+                if action_result == 2:
+                    result = fg.green + "Success"
+                    success = success + 1
+                elif action_result == 5:
+                    result = bg.red + fg.li_white + "Failure"
+                    failure = failure + 1
+                else:
+                    result = fg.cyan + "Unknown"
+                    unknown = unknown + 1
+
+                result = result + rs.all
+
+                print("|--> " + drive_serial + ' - ' + gigabytes + "GB; Result: ", end='')
+                print(result)
 
             try:
                 os.makedirs(output_directory)
             except:
                 pass
             file_exists = os.access(new_file, os.F_OK)
-            if file_exists and force_overwrite == False:
+            if file_exists and force_overwrite is False:
                 print(
-                    sty.fg.yellow + new_file + ' already exists and --force flag not sent. File not being output.' + sty.rs.all)
+                    fg.yellow + new_file + ' already exists and --force flag not sent. File not being output.' + rs.all)
             else:
                 os.rename(file_with_path, new_file)
 
     print("\n\nFinished Processing Files.")
-
+    total = success + failure + unknown
+    print("Successful: " + success)
+    print("Failed: " + failure)
+    if unknown > 0:
+        print("Unknown: " + unknown)
+    print("Total: " + total)
 
 if __name__ == "__main__":
     main()
